5b4a545ef8994493e214fbefc54eec2418af582a
Roman Suvorov <kikiwora@icloud.com>
2024-01-16 12:32:35 +0200

    version 1.0.2 — bump minimal supported iOS version to 15

--- Package.swift
+++ Package.swift
@@ -6,10 +6,9 @@ import PackageDescription
 let package = Package(
   name: "Convenient Concurrency",
   platforms: [
-    .iOS(.v13)
+    .iOS(.v15)
   ],
   products: [
-    // Products define the executables and libraries a package produces, making them visible to other packages.
     .library(
       name: "Convenient Concurrency",
       targets: ["Convenient Concurrency"]
@@ -20,8 +19,6 @@ let package = Package(
     .package(url: "https://github.com/Quick/Quick", from: "7.0.0")
   ],
   targets: [
-    // Targets are the basic building blocks of a package, defining a module or a test suite.
-    // Targets can depend on other targets in this package and products from dependencies.
     .target(
       name: "Convenient Concurrency"
     ),
@@ -32,4 +29,4 @@ let package = Package(
   ]
 )
 
-let packageVersion: Version = "1.0.0"
+let packageVersion: Version = "1.0.2"
